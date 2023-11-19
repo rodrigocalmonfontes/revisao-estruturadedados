b1faa0ae3eba6721bc4509f49b4040b38936f35d
Rodrigo Calmon <rodrigo.fontes@meutudo.app>
2023-11-19 19:35:39 -0300

    refatorando método de adicionar

--- src/main/java/org/example/vetoresEArrays/Vetor.java
+++ src/main/java/org/example/vetoresEArrays/Vetor.java
@@ -2,8 +2,10 @@ package org.example.vetoresEArrays;
 
 public class Vetor {
     private String[] elementos;
+    private int tamanho;
     public Vetor(int capacidade) {
         this.elementos = new String[capacidade];
+        this.tamanho = 0; //iniciado com 0 para ir incrementando
     }
 
 //     método adiciona:
@@ -11,8 +13,10 @@ public class Vetor {
 //     se for nula sigifnica que está disponível para poder usar e ai adicionamos nosso elemento
 //     nesta posição do vetor
 //     quer que o codigo pare de executar na 1a posi que encontrar null, por isso usamos break
-//     para frear o loop do for, e aí add o elemento no vetor.
-    public void adiciona(String elemento) {
+//     para frear o loop do for, e aí add o elemento no vetor
+//     este método não é performático, uma vez que é necessário percorrer elemento por elemento
+//     melhor controlar incluindo um novo atributo na classe para controlar o tamanho do vetor
+  /*  public void adiciona(String elemento) {
         for(int i = 0; i < elementos.length; i++) {
             if(this.elementos[i] == null) {
                 this.elementos[i] = elemento;
@@ -20,5 +24,13 @@ public class Vetor {
             }
         }
     }
-
+ */
+    public void adiciona(String elemento) throws Exception {
+        if(this.tamanho < this.elementos.length) { //se o tamanhhio é menor que a capacidade do vetor de elementos
+            this.elementos[this.tamanho] = elemento;
+            this.tamanho++;
+        } else {
+            throw new Exception("Vetor cheio");
+        }
+    }
 }
--- src/main/java/org/example/vetoresEArrays/VetoresEarrays.java
+++ src/main/java/org/example/vetoresEArrays/VetoresEarrays.java
@@ -2,11 +2,13 @@ package org.example.vetoresEArrays;
 
 public class VetoresEarrays {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         Vetor vetor = new Vetor(5);
 
         vetor.adiciona("elemento 1");
+        vetor.adiciona("elemento 2");
+        vetor.adiciona("elemento 3");
 
     }
 }
